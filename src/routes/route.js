bc6aabe3d5a816520837490df9b7e04a0d9473a2
dimplemahule <dimplemahule@gmail.com>
2022-06-02 14:28:02 +0530

    Node modules assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const logger = require('../logger/logger')
+const util = require('../util/helper')
+const externalmodule =require('../validator/formatter')
+const underscore = require('underscore')
+
+
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    logger.Welcome()
+    console.log("today date(dd/mm)-"+util.date()+ "/" +util.month())
+    console.log("Roadon, W3D1, the topic for today is Nodejs module system")
+    externalmodule.upper()
+    externalmodule.trime()
+    
     res.send('My first ever api!')
 });
 
