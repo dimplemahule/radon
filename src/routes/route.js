647bc39876c74a6aca621c3fdcf2a957432c82ea
dimplemahule <dimplemahule@gmail.com>
2022-06-09 14:25:42 +0530

    populate_reference

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,26 +3,28 @@ const router = express.Router();
 // const UserModel= require("../models/userModels")
 // const UserController= require("../controllers/userController")
 
-const bookModel= require("../models/bookModel")
-const bookAuthor= require("../models/AuthorModel")
-const Book= require("../controllers/Book")
+// const bookModel= require("../models/bookModel")
+// const bookAuthor= require("../models/AuthorModel")
+// const Book= require("../controllers/Book")
 
-
+const populateref = ("../controllers/populateref")
+const newAuthor = ("../models/newAuthor")
+const newBook = ("../models/newBook")
+const newPublisher = ("../models/newPublisher")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
 // router.post("/createUser", UserController.createUser  )
-
 // router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createAuthor", Book.createAuthor)
-router.post("/createBook", Book.createBook)
+// router.post("/createAuthor", Book.createAuthor)
+// router.post("/createBook", Book.createBook)
 
-router.get("/getBooksbyChetanBhat", Book.getBooksbyChetanBhat)
-router.post("/authorOfbooks", Book.authorOfbooks)
-router.get("/Finddata", Book.Finddata)
+// router.get("/getBooksbyChetanBhat", Book.getBooksbyChetanBhat)
+// router.post("/authorOfbooks", Book.authorOfbooks)
+// router.get("/Finddata", Book.Finddata)
 
 
 
