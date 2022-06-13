a9238820351706e94adf65eecf8aea51f3e5439c
dimplemahule <106535126+dimplemahule@users.noreply.github.com>
2022-06-13 15:51:43 +0530

    middleware2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,51 @@
-const UserModel= require("../models/userModel")
+const UseModel= require("../models/useModel")
+const productModel = require("../models/productModel")
+
+// const basicCode= async function(req, res, next) {
+//     let tokenDataInHeaders= req.headers.token
+//     console.log(tokenDataInHeaders)
+
+//     console.log( "HEADER DATA ABOVE")
+//     console.log( "hey man, congrats you have reached the Handler")
+//     //res.send({ msg: "This is coming from controller (handler)"})
+//     next()
+//     }
+
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let tokenDataInHeaders = req.headers.token
+//     console.log("Request headers befor modification",req.headers)
+//     console.log(req.headers.batch)
+//     console.log(req.headers["content-type"])
+//     req.headers['month']='jun'
+//     req.anything = "everything"
+//     console.log(tokenDataInHeaders)
+//     ///let savedData= await UserModel.create(data)
 
 
+//     //set a headers in response
+//     res.headers('year','2022')
+//     res.send({msg: "hiiii"})
+// }
 
-
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
+const createUse= async function (req, res) {
+        let data= req.body
+        let savedData= await UserModel.create(data)
+        res.send({msg: savedData})
+    }
+    const productUser= async function (req, res) {
+        let data= req.body
+        let productData= await UserModel.create(data)
+        res.send({msg: productData})
     }
 
 
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+module.exports.createUse= createUse
+module.exports.productUser= productUser
+//module.exports.getUsersData= getUsersData
+// module.exports.basicCode= basicCode
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,26 +1,21 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
+const UseModel= require("../models/useModel.js")
+const productModel = require("../models/productModel")
 const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+//const BookController= require("../controllers/bookController")
 const commonMW = require ("../middlewares/commonMiddlewares")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+//router.post("/createBook", BookController.createBook  )
 
+    router.post("/createUse", UserController.createUse)
+    router.post("/productUser",UserController.productUser)
 
-
-router.post("/createBook", BookController.createBook  )
-
-
-
-
-// router.post("/createUser", UserController.createUser  )
 // router.get("/getUsersData", UserController.getUsersData)
-
-
 // const mid1= function ( req, res, next) {
 //     console.log("Hi I am a middleware named Mid1")
 //     // logic
@@ -48,7 +43,7 @@ router.post("/createBook", BookController.createBook  )
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+//router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 
