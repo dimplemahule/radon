28643f7ca26bc02c98551cdad3579b5ce39e603c
dimplemahule <106535126+dimplemahule@users.noreply.github.com>
2022-06-14 21:48:04 +0530

    midd_auth

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
- 
-// const newAuthor = require("../models/newAuthor")
-// const newBook = require("../models/newBook")
-// const newPublisher = require("../models/newPublisher")
-//const bookController = require("../controllers/bookController")
+const userController = require("../controllers/userController")
+const userModel = require("../models/userModel")
+//const auth = require("../middelewere/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+router.post("/users", userController.createUser)
+router.post("/login", userController.loginUser)
 
-// router.post("/newAthourdata", bookController.newAthourdata)
-// router.post("/newBookData", bookController.newBookData)
-// router.post("/newPublisherdata", bookController.newPublisherdata)
-// router.get("/populateBook",bookController.populateBook)
-// router.put("/Upadatekey", bookController.Upadatekey)
-// router.put("/Upadateprice", bookController.Upadateprice)
+router.get("/users/:userId", userController.getUserData)
 
-
-
-
-
-
-
-
+router.delete("/users/:userId", userController.deleteUser)
+router.put("/users/:userId", userController.putUser)
 
 module.exports = router;
